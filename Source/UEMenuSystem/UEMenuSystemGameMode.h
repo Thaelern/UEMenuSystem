f62677741adf58895c04b2f026e9fe17e75afa6d
Lasse B. I. <lassebi92@gmail.com>
2024-10-14 18:08:29 +0200

    Initial commit

---
+++ Source/UEMenuSystem/UEMenuSystemGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEMenuSystemGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUEMenuSystemGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUEMenuSystemGameMode();
+};
+
+
+
